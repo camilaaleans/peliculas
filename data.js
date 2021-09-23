7e9df025cacd3f8233c6d9cbe2b2d8b5e466f065
Camila Aleans <89880704+camilaaleans@users.noreply.github.com>
2021-09-22 19:10:22 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,84 @@
+export const data = [
+    {
+        "id": 1,
+        "name": "Enter the void",
+        "director": "Gaspar Noe",
+        "year": "2009",
+        "image": "https://revistatuk.com/wp-content/uploads/2021/07/enter.jpg",
+        "price": 10000
+    },
+    {
+        "id": 2,
+        "name": "Matrix",
+        "director": "Lana Wachowski, Lilly Wachowski",
+        "year": "1999",
+        "image": "https://pics.filmaffinity.com/Matrix-155050517-mmed.jpg",
+        "price": 10000
+    },
+    {
+        "id": 3,
+        "name": "Clueless",
+        "director": "Amy Heckerling",
+        "year": "1995",
+        "image": "https://cinemasaturno.com/wp-content/uploads/2021/01/clueless.jpg",
+        "price": 10000
+    },
+    {
+        "id": 4,
+        "name": "Hellraiser",
+        "director": "Clive Barker",
+        "year": "1987",
+        "image": "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/4f2923b39635977d748fccae8bb066912d49678e23476188778fbdf83fb1f209._RI_V_TTW_.jpg",
+        "price": 10000
+    },
+    {
+        "id": 5,
+        "name": "Moonrise Kingdmon",
+        "director": "Wes Anderson",
+        "year": "2012",
+        "image": "https://indiehoy.com/wp-content/uploads/2021/07/moonrise-kingdom.jpg",
+        "price": 10000
+    },
+    {
+        "id": 6,
+        "name": "The Dreamers",
+        "director": "Bernardo Bertoluci",
+        "year": "2003",
+        "image": "https://m.media-amazon.com/images/M/MV5BY2QzYTUzZWUtODhjNy00NWNlLWI1NjUtZWQ3YmUwNjgzZjBmXkEyXkFqcGdeQXVyNjk1Njg5NTA@._V1_FMjpg_UX1000_.jpgmg9.jpg",
+        "price": 10000
+    },
+    {
+        "id": 7,
+        "name": "Pulp Fiction",
+        "director": "Quentin Tarantino",
+        "year": "1994",
+        "image": "https://m.media-amazon.com/images/I/81fQ6-8LHOL._SL1500_.jpg",
+        "price": 10000
+    },
+    {
+        "id": 8,
+        "name": "les amours imaginaires",
+        "director": "Xavier Dolan",
+        "year": "2010",
+        "image": "https://pics.filmaffinity.com/Los_amores_imaginarios-586038159-mmed.jpg",
+        "price": 10000
+    },
+    {
+        "id": 9,
+        "name": "Terminator",
+        "director": "James Cameron",
+        "year": "1985",
+        "image": "https://pics.filmaffinity.com/Terminator-741269996-large.jpg",
+        "price": 10000
+    },
+    {
+        "id": 10,
+        "name": "Back to the future",
+        "director": "Robert Zemeckis",
+        "year": "1985",
+        "image": "https://m.media-amazon.com/images/I/71sPGYXUl+L._AC_SY679_.jpg",
+        "price": 10000
+    }
+    
+
+     ]
